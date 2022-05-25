8e65d7360058ccbf510e9ef89aa242ac7b12b578
ed_bolma <ed.bolma21@gmail.com>
2022-05-25 13:25:44 +0200

    changed path to the dataBase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,27 @@ import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './Friends/FriendsList/FriendsList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
-import user from '../dataBase/user';
-import data from '../dataBase/data';
-import friends from '../dataBase/friends';
-import transactions from '../dataBase/transaction';
+import user from '../components/dataBase/user';
+import data from '../components/dataBase/data';
+import friends from '../components/dataBase/friends';
+import transactions from '../components/dataBase/transaction';
 
 const App = () => {
   return (
     <div>
-      
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
 
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
-      
+
       <TransactionHistory items={transactions} />
-      
     </div>
   );
 };
