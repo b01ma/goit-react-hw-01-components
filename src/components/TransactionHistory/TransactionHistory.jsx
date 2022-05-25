90ad2dd9fe8452c13662ecb8a868d3261d091065
ed_bolma <ed.bolma21@gmail.com>
2022-05-25 12:13:13 +0200

    checked for bugs

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,41 +3,32 @@ import TransactionData from './TransactionData';
 import propTypes from 'prop-types';
 
 const TransactionHistory = ({ items }) => {
-    return (
-        <table className={css.transactionHistory}>
-            <thead className={css.head}>
-                <tr >
-                    <th className={css.headItem}>Type</th>
-                    <th className={css.headItem}>Amount</th>
-                    <th className={css.headItem}>Currency</th>
-                </tr>
-            </thead>
-           
-            <tbody>
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.head}>
+        <tr>
+          <th className={css.headItem}>Type</th>
+          <th className={css.headItem}>Amount</th>
+          <th className={css.headItem}>Currency</th>
+        </tr>
+      </thead>
 
-                {items.map(item => 
-                <TransactionData
-                    key={item.id}
-                    type={item.type}
-                    amount={item.amount}
-                    currency={item.currency}
-                />
-             )}
-               
-            </tbody>
-        </table>
-    )
-}
+      <tbody>
+        {items.map(item => (
+          <TransactionData
+            key={item.id}
+            type={item.type}
+            amount={item.amount}
+            currency={item.currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 TransactionHistory.propTypes = {
-    items: propTypes.array.isRequired,
-}
+  items: propTypes.array.isRequired,
+};
 
 export default TransactionHistory;
-
-{/* <TransactionData
-                        key={item.id}
-                        type={item.type}
-                        amount={item.amount}
-                        currency={item.currency}
-                    // /> */}
